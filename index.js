641809f067d794b72d65b8f7a54973872f00ad1d
Ikenna <enebret@yahoo.com>
2023-07-14 15:16:27 +0100

    relevant edits

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const qrcode = require('qrcode-terminal');
 const express = require ('express');
 const app = express ();
+const x = require('x-ray')();
+const moment = require('moment-timezone');
 app.get('/', (req, res)=>{
   res.send ('first test of API')
 });
@@ -48,8 +50,92 @@ client.on("authenticated", (session) => {
       }
     };
     getChatHistory(client);
+    
     setInterval(() => {
-      client.sendMessage(chatId, text).then(msg=>console.log('message sent'));
+      async function scrapFunction() {
+        try {
+          async function extractl(q){
+            const g = x(q, '.unformatted-list', [
+             {
+               link: ['a@href']
+             }
+           ])
+             return g
+           }
+           
+           async function getb (){
+             let v = 'https://euraxess.ec.europa.eu/funding/search?page=1' //for eg
+             var linkarr = ['https://euraxess.ec.europa.eu/funding/search?','https://euraxess.ec.europa.eu/funding/search?page=1','https://euraxess.ec.europa.eu/funding/search?page=2','https://euraxess.ec.europa.eu/funding/search?page=3','https://euraxess.ec.europa.eu/funding/search?page=4','https://euraxess.ec.europa.eu/funding/search?page=5','https://euraxess.ec.europa.eu/funding/search?page=6','https://euraxess.ec.europa.eu/funding/search?page=7']
+             var rpageIndex = Math.floor(Math.random() * linkarr.length);
+             var rp = linkarr[rpageIndex];
+             let f = await extractl(rp);//randomize the page links and input one here
+             let links = f[0].link;
+             var rIndex = Math.floor(Math.random() * links.length);
+             var qp = links[rIndex];
+             const z = x(qp, '.dialog-off-canvas-main-canvas', [{
+               head: 'h1',
+               deadline: '.ecl-description-list__definition :nth-child(1)',
+              link: ['.ecl-description-list__definition a@href']
+             }])(function(err, db){
+               //console.log(db)
+             })
+             return z
+           }
+           //check https://euraxess.ec.europa.eu/user/login?destination=/taxonomy/term/779
+             //check for date also
+           async function et (){
+             let data = await getb();
+             let dl = data[0];
+             let dateString = data[0].deadline;
+             let dt = data[0].link[1];
+             let timeZone = 'Africa/Lagos'; // Nigeria timezone
+             let dateObj = moment.tz(dateString, 'DD MMMM YYYY', timeZone);
+             let now = moment.tz(timeZone);
+           
+             let qr = {
+               head: 'AVAILABLE FUNDED POSITION',
+               title: dl.head,
+               deadline: dl.deadline,
+               link: dl.link[1]
+               
+             }
+           
+             let qf = qr.title+qr.link+qr.deadline
+             if (data[0].deadline==undefined) {
+                //console.log(dl)
+                 return await et();
+             }
+             
+              if (!dt) {
+                //console.log(dl)
+                 return await et();
+             }
+           
+             if(dateObj.isBefore(now, 'day')){
+               return await et();
+             }
+           
+             if(qf.length>250){
+               return await et();
+             }
+           
+             if(qr.link===undefined){
+               return await et();
+             }
+             //return dl;
+             
+             return qr
+           }
+               const obj = await et();
+              let ad = '\n\n'+'𝐆𝐫𝐨𝐮𝐩 𝐈𝐧𝐯𝐢𝐭𝐞 𝐋𝐢𝐧𝐤: https://t.me/+mRKfd_GxHwNmNDk0'+'\n\n'
+             let r = obj.head+'\n'+obj.title+ad+'Deadline: '+obj.deadline+'\n'+obj.link
+    
+          client.sendMessage(chatId, r).then(msg=>console.log('message sent'));
+        } catch (error) {
+          console.log(error)
+        }
+      };
+scrapFunction();
     }, 6000);
     //getUnreadMsg(client);
 });
